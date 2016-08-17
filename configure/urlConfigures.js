4894f53689e2871defa1f666af81869b314c7845
winty <18814127231@163.com>
2016-08-17 23:56:06 +0800

    try connect to db

---
+++ configure/urlConfigures.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2016/8/17.
+ */
+var urlConfig={
+    dbUrl:'mongodb://localhost/networkLearning'
+}
+
+module.exports=urlConfig;
---
+++ server/db.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Administrator on 2016/8/17.
+ */
+var mongoose=require('mongoose');
+var dbUrl=require('../configure/urlConfigures');
+
+mongoose.connection.on('connected', function () {
+    console.log('Mongoose connected to ' + dbUrl.dbUrl);
+});
+mongoose.connection.on('error',function (err) {
+    console.log('Mongoose connection error: ' + err);
+});
+mongoose.connection.on('disconnected', function () {
+    console.log('Mongoose disconnected');
+});
