d037a0ce407beb57343082cc5e01bb91e051f79b
winty <18814127231@163.com>
2016-08-26 23:12:41 +0800

    change file name

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var login = require('./routes/login');
+var regist = require('./routes/regist');
 
 var app = express();
 
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'node_modules')));
 
-app.use('/', login);
+app.use('/', regist);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
