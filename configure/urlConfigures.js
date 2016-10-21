d7c2e75bb0b1bc6891c926980f4e11c04e433194
winty <18814127231@163.com>
2016-10-21 21:08:49 +0800

    start picture save

--- configure/urlConfigures.js
+++ configure/urlConfigures.js
@@ -2,7 +2,9 @@
  * Created by Administrator on 2016/8/17.
  */
 var urlConfig={
-    dbUrl:'mongodb://localhost/networkLearning'
+    dbUrl:'mongodb://localhost/networkLearning',
+    dbServer:'127.0.0.1',
+    dbPort:'27017'
 }
 
 module.exports=urlConfig;
--- model/model.js
+++ model/model.js
@@ -2,10 +2,14 @@
  * Created by winty on 2016/8/16.
  */
 var mongoose=require('mongoose');
+var grid=require('gridfs-stream');
+var dbUrl=require('../configure/urlConfigures');
 
 var UserSchema=new mongoose.Schema({
     account:String,
     password:String
 });
+
+
 
 mongoose.model('User', UserSchema);
