f2dee07af33e78da0b0f6d7c7ee0ae67906b4e3a
winty <18814127231@163.com>
2016-10-21 23:48:58 +0800

    start picture save

--- model/model.js
+++ model/model.js
@@ -2,13 +2,29 @@
  * Created by winty on 2016/8/16.
  */
 var mongoose=require('mongoose');
-var grid=require('gridfs-stream');
 var dbUrl=require('../configure/urlConfigures');
 
 var UserSchema=new mongoose.Schema({
     account:String,
     password:String
 });
+
+//存储图片文件
+mongoose.connect(dbUrl.dbUrl);
+var conn=mongoose.connection;
+
+var fs=require('fs');
+var grid=require('gridfs-stream');
+grid.mongo=mongoose.mongo;
+
+conn.once('open',function(){
+    console.log('已经open了');
+    var gfs=grid(conn.db);
+
+    var writestream=gfs.createWriteStream({
+        filename:' '
+    })
+})
 
 
 
