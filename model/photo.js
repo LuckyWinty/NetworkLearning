ba6328b04554b23416bd82358b161804c26ca358
winty <zhouwenting1@jd.com>
2016-11-17 16:44:35 +0800

    start picture save

--- model/photo.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by winty on 2016/11/14.
- */
-var mongoose = require('mongoose');
-
-mongoose.connect(dbUrl.dbUrl); //连接本地photo_app数据库
-var schema = new mongoose.Schema({name: String, path: String});
-module.exports = mongoose.model('Photo', schema);
