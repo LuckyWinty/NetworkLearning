8d47fd46e49a0a0a9054de8972356c8ece49a4fd
winty <zhouwenting1@jd.com>
2016-11-18 17:19:04 +0800

    start picture save

--- model/model.js
+++ model/model.js
@@ -10,22 +10,26 @@ var UserSchema=new mongoose.Schema({
 });
 
 //存储图片文件
-mongoose.connect(dbUrl.dbUrl);
-var conn=mongoose.connection;
-
-var fs=require('fs');
-var grid=require('gridfs-stream');
-grid.mongo=mongoose.mongo;
-
-conn.once('open',function(){
-    console.log('已经open了');
-    var gfs=grid(conn.db);
-
-    var writestream=gfs.createWriteStream({
-        filename:'essayInfo'
-    })
-})
-
+//mongoose.connect(dbUrl.dbUrl);
+//var conn=mongoose.connection;
+//
+//var fs=require('fs');
+//var grid=require('gridfs-stream');
+//grid.mongo=mongoose.mongo;
+//
+//conn.once('open',function(){
+//    console.log('已经open了');
+//    var gfs=grid(conn.db);
+//
+//    var writestream=gfs.createWriteStream({
+//        filename:'essayPic'
+//    });
+//
+//    writestream.on('close', function (file) {
+//        console.log(file);
+//    });
+//    fs.createReadStream('public/images/31.jpg').pipe(writestream);
+//})
 
 
 mongoose.model('User', UserSchema);
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,25 @@ var router = express.Router();
 var registController = require("../controllers/registController");
 var loginController = require("../controllers/loginController");
 
+var fs = require('fs');
+var mongoose = require("mongoose");
+var Grid = require('gridfs-stream');
+var GridFS = Grid(mongoose.connection.db, mongoose.mongo);
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  try {
+//read from mongodb
+    GridFS.files.find({ filename: 'essayPic' }).toArray(function (err, files) {
+      if (err) {
+        throw (err);
+      }
+      console.log(files);
+    });
+  } catch (err) {
+    console.log('444');
+  }
   res.render('index', { title: 'Express' });
 });
 
