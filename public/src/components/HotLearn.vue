3c8cc718ba156bd9fc7f921f1f8b4a969182a57b
zhouwenting6 <zhouwenting1@jd.com>
2016-12-14 11:25:30 +0800

    include frameworks

---
+++ public/src/components/HotLearn.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hot">
+      <h1 class="ui header">{{msg}}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'hello',
+    data () {
+      return {
+        msg: 'Welcome to learning network!'
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
