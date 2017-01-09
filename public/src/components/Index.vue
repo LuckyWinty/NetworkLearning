15d80ad40ab7b1135f149328d795912e31f0fda8
winty <18814127231@163.com>
2017-01-09 23:39:34 +0800

    add

--- public/src/components/Index.vue
+++ public/src/components/Index.vue
@@ -5,7 +5,171 @@
       <el-button class="search-btn" type="primary" icon="search">搜索</el-button>
     </div>
     <div class="learn-guide">
-
+      <ul class="guide-ul">
+          <li>
+              <h4>网络安全学习</h4>
+              <div class="rhombus">
+                <ul class="content-list">
+                <li><a href="#">开始学习1</a></li>
+                <li><a href="#">开始学习2</a></li>
+                <li><a href="#">开始学习3</a></li>
+                <li><a href="#">开始学习4</a></li>
+                </ul>
+               <div class="line">
+                 <span class="long-line"></span>
+                 <span class="diamonds"></span>
+                 <span class="short-line"></span>
+               </div>
+              </div>
+          </li>
+                    <li>
+              <h4>网络安全学习</h4>
+              <div class="rhombus">
+                <ul class="content-list">
+                <li><a href="#">开始学习1</a></li>
+                <li><a href="#">开始学习2</a></li>
+                <li><a href="#">开始学习3</a></li>
+                <li><a href="#">开始学习4</a></li>
+                </ul>
+               <div class="line">
+                 <span class="long-line"></span>
+                 <span class="diamonds"></span>
+                 <span class="short-line"></span>
+               </div>
+              </div>
+          </li>
+                    <li>
+              <h4>网络安全学习</h4>
+              <div class="rhombus">
+                <ul class="content-list">
+                <li><a href="#">开始学习1</a></li>
+                <li><a href="#">开始学习2</a></li>
+                <li><a href="#">开始学习3</a></li>
+                <li><a href="#">开始学习4</a></li>
+                </ul>
+               <div class="line">
+                 <span class="long-line"></span>
+                 <span class="diamonds"></span>
+                 <span class="short-line"></span>
+               </div>
+              </div>
+          </li>
+                    <li>
+              <h4>网络安全学习</h4>
+              <div class="rhombus">
+                <ul class="content-list">
+                <li><a href="#">开始学习1</a></li>
+                <li><a href="#">开始学习2</a></li>
+                <li><a href="#">开始学习3</a></li>
+                <li><a href="#">开始学习4</a></li>
+                </ul>
+               <div class="line">
+                 <span class="long-line"></span>
+                 <span class="diamonds"></span>
+                 <span class="short-line"></span>
+               </div>
+              </div>
+          </li>
+                    <li>
+              <h4>网络安全学习</h4>
+              <div class="rhombus">
+                <ul class="content-list">
+                <li><a href="#">开始学习1</a></li>
+                <li><a href="#">开始学习2</a></li>
+                <li><a href="#">开始学习3</a></li>
+                <li><a href="#">开始学习4</a></li>
+                </ul>
+               <div class="line">
+                 <span class="long-line"></span>
+                 <span class="diamonds"></span>
+                 <span class="short-line"></span>
+               </div>
+              </div>
+          </li>
+      </ul>
+      <div class="cut-off"></div>
+            <ul class="guide-ul">
+          <li>
+              <h4>网络安全学习</h4>
+              <div class="rhombus">
+                <ul class="content-list">
+                <li><a href="#">开始学习1</a></li>
+                <li><a href="#">开始学习2</a></li>
+                <li><a href="#">开始学习3</a></li>
+                <li><a href="#">开始学习4</a></li>
+                </ul>
+               <div class="line">
+                 <span class="long-line"></span>
+                 <span class="diamonds"></span>
+                 <span class="short-line"></span>
+               </div>
+              </div>
+          </li>
+                    <li>
+              <h4>网络安全学习</h4>
+              <div class="rhombus">
+                <ul class="content-list">
+                <li><a href="#">开始学习1</a></li>
+                <li><a href="#">开始学习2</a></li>
+                <li><a href="#">开始学习3</a></li>
+                <li><a href="#">开始学习4</a></li>
+                </ul>
+               <div class="line">
+                 <span class="long-line"></span>
+                 <span class="diamonds"></span>
+                 <span class="short-line"></span>
+               </div>
+              </div>
+          </li>
+                    <li>
+              <h4>网络安全学习</h4>
+              <div class="rhombus">
+                <ul class="content-list">
+                <li><a href="#">开始学习1</a></li>
+                <li><a href="#">开始学习2</a></li>
+                <li><a href="#">开始学习3</a></li>
+                <li><a href="#">开始学习4</a></li>
+                </ul>
+               <div class="line">
+                 <span class="long-line"></span>
+                 <span class="diamonds"></span>
+                 <span class="short-line"></span>
+               </div>
+              </div>
+          </li>
+                    <li>
+              <h4>网络安全学习</h4>
+              <div class="rhombus">
+                <ul class="content-list">
+                <li><a href="#">开始学习1</a></li>
+                <li><a href="#">开始学习2</a></li>
+                <li><a href="#">开始学习3</a></li>
+                <li><a href="#">开始学习4</a></li>
+                </ul>
+               <div class="line">
+                 <span class="long-line"></span>
+                 <span class="diamonds"></span>
+                 <span class="short-line"></span>
+               </div>
+              </div>
+          </li>
+                    <li>
+              <h4>网络安全学习</h4>
+              <div class="rhombus">
+                <ul class="content-list">
+                <li><a href="#">开始学习1</a></li>
+                <li><a href="#">开始学习2</a></li>
+                <li><a href="#">开始学习3</a></li>
+                <li><a href="#">开始学习4</a></li>
+                </ul>
+               <div class="line">
+                 <span class="long-line"></span>
+                 <span class="diamonds"></span>
+                 <span class="short-line"></span>
+               </div>
+              </div>
+          </li>
+      </ul>
     </div>
   <h3 class="hot_head">最热学习榜<el-button class="float-right"  type="primary">查看更多</el-button></h3>
     <Subject></Subject>
@@ -20,12 +184,85 @@
     width:50%;
     margin:20px auto;
   }
-  .search:after{
+  .search:after,  .learn-guide .guide-ul:after{
     display: block;
     content: '';
     visibility: hidden;
     clear: both;
   }
+  .learn-guide{
+   width:100%;
+   height:490px;
+   margin-bottom:10px;
+   background-image:url('../images/banner1.jpg');
+   background-repeat:no-repeat;
+   background-size:100% 100%;
+  }
+  .learn-guide .guide-ul li{
+  float:left;
+  margin-left:50px;
+  margin-top:50px;
+  }
+  .rhombus{
+   position:relative;
+   width:200px;
+   height:120px;
+   border:solid 1px #fff;
+   border-bottom: none;
+   margin-top: 5px;
+  }
+  .learn-guide .rhombus .content-list li{
+    float:none;
+    margin: 5px;
+  }
+    .learn-guide .rhombus .content-list li a{
+      color:#fff;
+    }
+  .line{
+    position: absolute;
+    bottom: -5px;
+  }
+  .rhombus .long-line{
+    display: block;
+    float:left;
+    width:120px;
+    height:1px;
+    background-color: #fff;
+    margin-right:30px;
+  }
+  .rhombus .diamonds{
+    display: block;
+    float:left;
+    width:0;
+    border-width:5px;
+    border-color: #fff;
+    border-style: solid;
+    margin-top: -5px;
+  }
+  .rhombus .short-line{
+    display: block;
+    float:left;
+    width:40px;
+    height:1px;
+    background-color: #fff;
+  }
+  .cut-off{
+    width:100%;
+    border-bottom: solid 1px #fff;
+    margin-top: 50px;
+  }
+ /* .triangle-top{
+   width:0;
+   border-width:100px;
+   border-color:transparent transparent #fff transparent;
+   border-style:dashed dashed solid dashed;
+  }*/
+  /*.rectangle{
+  width:200px;
+  height:200px;
+  border:solid 1px #fff;
+  transform:rotate(45deg);
+  }*/
   .inline{
     width:78%;
     float:left;
