38bf18df9a12e87674b35121fb482b1ac5862b9b
winty <18814127231@163.com>
2017-02-24 21:57:25 +0800

    不知道是啥

---
+++ controllers/showInfoController.js
@@ -0,0 +1,43 @@
+/**
+ * Created by winty on 2017/2/23.
+ */
+
+var mongoose = require('mongoose');
+require('../model/model');
+var User = mongoose.model('User');
+var Subject = mongoose.model('Subject');
+var mongo = require('mongodb');
+var util = require('util');
+var moment=require('moment');
+
+module.exports.showSubjects = function(req, res){
+    Subject.find({})
+        .sort({'created':-1})
+        .exec(function(error,Subjects){
+            if(error){
+                console.log('.....查找所有课程出错',error);
+            }else{
+                res.json({status: 1,Subjects:Subjects, mes: '查找所有课程成功！'});
+            }
+        })
+}
+
+module.exports.getImage = function(req, res){
+    var _id = new mongo.ObjectId(req.query.imageId);
+    console.log("_id--------------------" , _id);
+    gfs.files.findOne({'_id': _id}, function (err, file) {
+        console.log("pic--------------------" , file);
+        if (err) return res.status(400).send(err);
+        if (!file) return res.status(404).send('');
+        res.set('Content-Type', file.contentType);
+        res.set('Content-Disposition', 'attachment; filename=""');
+        var readstream = gfs.createReadStream({
+            _id: file._id
+        });
+        readstream.on("error", function(err) {
+            console.log("Got error while processing stream " + err.message);
+            res.end();
+        });
+        readstream.pipe(res);
+    });
+}
